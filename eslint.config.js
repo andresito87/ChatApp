3e7d576d8528ebce706d3de202b103462582a9ec
Andres <andrespodadera87@gmail.com>
2024-08-17 15:40:19 +0200

    adding testting and fixing dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,7 @@
-'use strict'
-
-const standard = require('eslint-config-standard')
-module.exports = [
-  standard,
-  {
-    extends: ["@hono/eslint-config", "plugin:prettier/recommended", "prettier","standard"],
-    plugins: ["prettier"],
-    rules: {
-      "prettier/prettier": "error",
-    }
-  }
-]
+module.exports = {
+  extends: ["@hono/eslint-config", "plugin:prettier/recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
